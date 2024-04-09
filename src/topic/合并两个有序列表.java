a2e8f4946aae1bb97fa736e8ad5d8d63fb68381c
usylvbu <www.1979130746@qq.com>
2024-04-09 16:45:20 +0800

    第二十一题合并两个有序列表

---
+++ src/topic/合并两个有序列表.java
@@ -0,0 +1,56 @@
+package topic;
+/*
+第二十一题合并两个有序列表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+示例 1：
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+提示：
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+ */
+public class 合并两个有序列表 {
+    public Solution.ListNode mergeTwoLists(Solution.ListNode list1, Solution.ListNode list2) {
+        Solution.ListNode result =  new Solution.ListNode();
+        Solution.ListNode next = result;
+        if (list1 == null && list2 != null){
+            return list2;
+        } else if (list1 !=null && list2 == null) {
+            return list1;
+        }
+        if (list1 == null && list2 == null){
+            return null;
+        }
+        while (list1 != null || list2 != null){
+
+            if (list1.val < list2.val){
+                next.val = list1.val;
+                list1 = list1.next;
+            }else{
+                next.val = list2.val;
+                list2 = list2.next;
+            }
+            if (list1 == null){
+                next.next = list2;
+                break;
+            }else if (list2 == null){
+                next.next = list1;
+                break;
+            }
+            next.next = new Solution.ListNode();
+            next = next.next;
+        }
+
+        return result;
+    }
+}
